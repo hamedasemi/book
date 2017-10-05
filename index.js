01abb8eed17a261f5e888e59564a1aeedb93f195
hamedasemi <h-@live.se>
2017-10-05 17:21:49 +0200

    Works all

--- index.js
+++ index.js
@@ -5,16 +5,11 @@ const app = express()
 app.get('/verses', function(req, res) {
     MongoClient.connect('mongodb://user:pass@ds147544.mlab.com:47544/book', function(err, db) {
 
-        db.collection('en-itani', (err, collection) => {
-            collection.find({}, { skip: 1, limit: 10 }).toArray((err, data) => {
+        db.collection('verses', (err, collection) => {
+            collection.find({}, { _id: 0, 'en-itani': 1, 'fa-makarem': 1 }, { skip: 1, limit: 10 }).toArray((err, data) => {
                 res.json(data)
             })
         })
-
-
-
-
-
     })
 
 })
